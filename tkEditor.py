0adf969d2b09a3cad37b880c6b6bad601ec13641
dante <zhivchikovv2@gmail.com>
2019-09-26 22:37:24 +0300

    add menu templates, add fullscreen mode

--- tkEditor.py
+++ tkEditor.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from tkinter import *
+from tkinter.ttk import *
 from tkinter import messagebox as mb
 from tkinter import filedialog as fd
 def openr():
@@ -11,9 +12,9 @@ def openr():
         b.insert(1.0,s)
         file.close()
     except:
-        mb.showerror("Ошибка!", "Ошибка)")
+        mb.showerror("Error!", "Error")
 def text_delete():
-    delete=mb.askyesno(title="Вы уверены?", message="Вы уверены?")
+    delete=mb.askokcancel(title="Are you sure?", message="Are you sure?")
     if delete==True:
         b.delete(0.0, END)
 def save():
@@ -24,20 +25,82 @@ def save():
         file.write(s)
         file.close()
     except:
-        mb.showerror("Ошибка!", "ошибка)")
+        mb.showerror("Error", "Error")
+
+strr = ["Dark theme", "Light theme"]
+
+
+
+
+
+
+
+
+
+
+
 root=Tk()
+
+def full():
+  try:
+    root.overrideredirect(True)
+    root.overrideredirect(False)
+    root.attributes('-fullscreen',True)
+  except:
+    mb.showerror("Error!", "Error")
+main_menu = Menu()
+"""file works """
+file_menu = Menu()
+file_menu.add_command(label="New")
+file_menu.add_command(label="Save", command = openr)
+file_menu.add_command(label="Open")
+file_menu.add_separator()
+file_menu.add_command(label="Exit")
+     
+
+"""Edit menu """
+file_edit = Menu()
+file_edit.add_command(label="Cut", command = text_delete)
+
+
+"""View menu """
+view_menu = Menu()
+
+view_menu.add_command(label = "Change theme")
+view_menu.add_command(label = "Change language")
+view_menu.add_command(label = "Change font")
+view_menu.add_command(label = "Enter fullscreen mode", command = full)
+
+"""main menu"""
+main_menu.add_cascade(label="File", menu=file_menu)
+main_menu.add_cascade(label="Edit", menu=file_edit)
+main_menu.add_cascade(label="View", menu=view_menu)
+ 
+root.config(menu=main_menu)
+
+
+
 root.title("Dante's text editor")
-a=Button(text="Выбрать файл", bg="grey", command=openr)
-b=Text(width=100,
-       height=25,
+
+
+"""a=Button(text="Выбрать файл", bg="grey", command=openr)"""
+b=Text(width=250,
+       height=10,
        bg="grey",
        fg="white",
        wrap=WORD)
-c=Button(text="Очистить", bg="grey", command=text_delete)
+alt=Text(width=250,
+         height=10,
+         bg="white",
+         fg="black",
+         wrap=WORD)
+"""c=Button(text="Очистить", bg="grey", command=text_delete)
 d=Button(text="Сохранить", bg="grey", command=save)
-a.pack()
+a.pack()"""
 b.pack()
-c.pack()
-d.pack()
+alt.pack()
+"""c.pack()
+d.pack()"""
+
 
 root.mainloop()
